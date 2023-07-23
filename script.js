41e855cbf911e387baebf3bcdd628fe5eb130058
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-07-23 13:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+ function firstNonRepeatingCharacter(str) {
+      const frequencyMap = {};
+
+      // Count the occurrences of each character
+      for (let char of str) {
+        frequencyMap[char] = (frequencyMap[char] || 0) + 1;
+      }
+
+      // Find the first character with frequency 1
+      for (let char of str) {
+        if (frequencyMap[char] === 1) {
+          return char;
+        }
+      }
+
+      return null; // Return null if no non-repeating character found
+    }
+
+    // Test cases
+    const input = prompt("Enter a string:");
+    const result = firstNonRepeatingCharacter(input);
+    if (result) {
+      console.log("First non-repeating character:", result);
+    } else {
+      console.log("No non-repeating character found.");
+    }
